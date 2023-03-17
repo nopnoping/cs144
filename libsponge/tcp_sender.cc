f4437fab8944f646535bc3fbc190295e288eba98
luexp <10457192@qq.com>
2023-03-17 14:16:36 +0800

    修改一下tcp_sender发送的空segment

--- libsponge/tcp_sender.cc
+++ libsponge/tcp_sender.cc
@@ -114,7 +114,6 @@ unsigned int TCPSender::consecutive_retransmissions() const {
 
 void TCPSender::send_empty_segment() {
     TCPSegment segment;
-    segment.header().syn = true;
     segment.header().seqno = wrap(_next_seqno, _isn);
     _segments_out.push(segment);
 }
